1e5bcfe584b3b36abd524292786a6027c7bfbe8a
Gabriel Martins <gabrielmartins0898@gmail.com>
2022-03-15 17:59:41 -0400

    adicionando comportamento de visible e hidden no submenu atraves do js e DOM

---
+++ js/polo-visivel.js
@@ -0,0 +1,17 @@
+const subMenuPolo = document.querySelector('.submenu-polo')
+const menuNome = document.querySelector('.menu-polo');
+
+const subShow = () => {
+    subMenuPolo.style.visibility = 'visible';
+}
+
+const subHidden = () => {
+    subMenuPolo.style.visibility = 'hidden';
+}
+
+
+menuNome.addEventListener('mouseenter', subShow);
+menuNome.addEventListener('mouseleave', () =>{
+    setTimeout(subHidden, 3000);
+})
+
